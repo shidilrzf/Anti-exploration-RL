ad47a1bdea81a54c1e12bb3a6e7d29933ff4a640
shidilrzf <shideh.rezaeifar@unige.ch>
2021-01-22 08:45:15 +0100

    added MLP with embeddings to the networks

--- rlkit/torch/networks.py
+++ rlkit/torch/networks.py
@@ -78,6 +78,77 @@ class Mlp(nn.Module):
             return output
 
 
+class Mlp_embedding(nn.Module):
+    def __init__(
+            self,
+            hidden_sizes,
+            embedding_sizes,
+            output_size,
+            input_sizes,
+            init_w=3e-3,
+            hidden_activation=F.relu,
+            output_activation=nn.Identity(),
+            hidden_init=ptu.fanin_init,
+            b_init_value=0.1,
+            layer_norm=False,
+            layer_norm_kwargs=None,
+    ):
+        super().__init__()
+
+        if layer_norm_kwargs is None:
+            layer_norm_kwargs = dict()
+
+        self.input_sizes = input_sizes
+        self.output_size = output_size
+
+        self.embedding_sizes = embedding_sizes
+        self.hidden_activation = hidden_activation
+        self.output_activation = output_activation
+        self.layer_norm = layer_norm
+        self.fcs = []
+        self.layer_norms = []
+#         in_size = input_size
+
+        self.embed1 = nn.Linear(input_sizes[0], embedding_sizes[0])
+        self.embed2 = nn.Linear(input_sizes[1], embedding_sizes[1])
+
+        in_size = embedding_sizes[0] + embedding_sizes[1]
+
+        for i, next_size in enumerate(hidden_sizes):
+            fc = nn.Linear(in_size, next_size)
+            in_size = next_size
+            hidden_init(fc.weight)
+            fc.bias.data.fill_(b_init_value)
+            self.__setattr__("fc{}".format(i), fc)
+            self.fcs.append(fc)
+
+            if self.layer_norm:
+                ln = LayerNorm(next_size)
+                self.__setattr__("layer_norm{}".format(i), ln)
+                self.layer_norms.append(ln)
+
+        self.last_fc = nn.Linear(in_size, output_size)
+        self.last_fc.weight.data.uniform_(-init_w, init_w)
+        self.last_fc.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, obs, act, return_preactivations=False):
+        h1 = F.relu(self.embed1(obs))
+        h2 = F.relu(self.embed2(act))
+
+        h = torch.cat((h1, h2), dim=-1)
+        for i, fc in enumerate(self.fcs):
+            h = fc(h)
+            if self.layer_norm and i < len(self.fcs) - 1:
+                h = self.layer_norms[i](h)
+            h = self.hidden_activation(h)
+        preactivation = self.last_fc(h)
+        output = self.output_activation(preactivation)
+        if return_preactivations:
+            return output, preactivation
+        else:
+            return output
+
+
 class FlattenMlp(Mlp):
     """
     Flatten inputs along dimension 1 and then pass through MLP.
@@ -119,5 +190,6 @@ class TanhMlpPolicy(MlpPolicy):
     """
     A helper class since most policies have a tanh output activation.
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, output_activation=torch.tanh, **kwargs)
