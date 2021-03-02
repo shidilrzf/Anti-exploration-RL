7197a09edf43119655e4e4b9a859dc1c92fb26f9
shidilrzf <shideh.rezaeifar@unige.ch>
2021-03-02 19:09:43 +0100

    changed the HP

--- examples/sac_bonus_ae.py
+++ examples/sac_bonus_ae.py
@@ -195,8 +195,8 @@ if __name__ == "__main__":
     parser.add_argument('--num_epochs', default=2000, type=int)
 
     # sac
-    parser.add_argument('--qf_lr', default=3e-4, type=float)
-    parser.add_argument('--policy_lr', default=1e-4, type=float)
+    parser.add_argument('--qf_lr', default=1e-4, type=float)
+    parser.add_argument('--policy_lr', default=3e-4, type=float)
     parser.add_argument('--num_samples', default=100, type=int)
     parser.add_argument('--no_automatic_entropy_tuning', action='store_true', default=False, help='no automatic entropy tuning')
 
